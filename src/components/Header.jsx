077cbee43e8feca4a5333f27c94d53db9f34a3ed
Animish2002 <animishchopade123@gmail.com>
2024-12-04 10:06:45 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import logo from "../assets/V-Gold.png";
 
 export default function Header() {
   return (
-    <Navbar   className="bg-[#29277a]">
-      <Navbar.Brand href="https://flowbite-react.com">
+    <Navbar className="bg-[#29277a]">
+      <Navbar.Brand >
         <img
           src={logo}
           className="mr-3 h-10 sm:h-14"
@@ -13,23 +13,45 @@ export default function Header() {
         <span className="self-center whitespace-nowrap text-xl font-semibold md:text-white"></span>
       </Navbar.Brand>
       <div className="flex md:order-2 ">
-        <Button className="text-lg ">Get started</Button>
+        <Button className="text-lg bg-[#FF6B00] text-black ">
+          Get started
+        </Button>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link className="text-lg md:text-white hover:text-yellow-300 " href="#" active>
+        <Navbar.Link
+          className="text-lg md:text-white hover:text-yellow-300 "
+          href="#"
+          active
+        >
           Home
         </Navbar.Link>
-        <Navbar.Link href="#" className="text-lg md:text-white hover:text-yellow-300" active>
+        <Navbar.Link
+          href="#"
+          className="text-lg md:text-white hover:text-yellow-300"
+          active
+        >
           About
         </Navbar.Link>
-        <Navbar.Link href="#" className="text-lg md:text-white hover:text-yellow-300" active>
+        <Navbar.Link
+          href="#"
+          className="text-lg md:text-white hover:text-yellow-300"
+          active
+        >
           Services
         </Navbar.Link>
-        <Navbar.Link href="#" className="text-lg md:text-white hover:text-yellow-300" active>
+        <Navbar.Link
+          href="#"
+          className="text-lg md:text-white hover:text-yellow-300"
+          active
+        >
           Pricing
         </Navbar.Link>
-        <Navbar.Link href="#" className="text-lg md:text-white hover:text-yellow-300" active>
+        <Navbar.Link
+          href="#"
+          className="text-lg md:text-white hover:text-yellow-300"
+          active
+        >
           Contact
         </Navbar.Link>
       </Navbar.Collapse>
